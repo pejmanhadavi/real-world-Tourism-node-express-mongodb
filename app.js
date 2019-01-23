6b33e139c0c2e65ffcdb52e6b099e6e962b2096a
Pejman Hadavi <pejmanhadaviph@yahoo.com>
2019-01-23 20:50:52 +0330

    Take out views from app

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ mongoose.connect(config.get('database'), {useNewUrlParser: true})
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'app/views'));
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 // use middleware
