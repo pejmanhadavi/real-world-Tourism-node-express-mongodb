5fccb6dae9584802b9efc03e5103e1103030174d
Pejman Hadavi <pejmanhadaviph@yahoo.com>
2019-01-25 16:54:46 +0330

    complete auth.js route for register

--- app/routes/auth.js
+++ app/routes/auth.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const validate = require('../controllers/auth.validate');
 const controller = require('../controllers/auth');
+
+const User = require('../models/user').User;
 const router = express.Router();
 
 /*
