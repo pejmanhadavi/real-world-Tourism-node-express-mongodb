b8a191b68760844bbace6a62f4f9df618687d312
Pejman Hadavi <pejmanhadaviph@yahoo.com>
2019-03-17 16:09:05 +0330

    add experience data

---
+++ data/experiences/experience.js
@@ -0,0 +1,24 @@
+const faker = require('faker');
+const ObjectId = require('mongoose').Types.ObjectId;
+
+
+module.exports = [
+    {
+    _id: new ObjectId('5c8e33466c3165282fbcc2ab'),
+    title: 'EX1',
+    cost: 80000,
+    description: 'this is some description',
+    },
+    {
+        _id: new ObjectId('5c8e342f7bcd6d2b56114945'),
+        title: 'EX2',
+        cost: 20000,
+        description: 'this is some description',
+    },
+    {
+        _id: new ObjectId('5c8e343680ee502b6704abfe'),
+        title: 'EX3',
+        cost: 50000,
+        description: 'this is some description',
+    },
+];
--- data/tourLeaders/tourLeader.js
+++ data/tourLeaders/tourLeader.js
@@ -4,7 +4,7 @@ const ObjectId = require('mongoose').Types.ObjectId;
 module.exports =[
     {
         _id:new ObjectId('5c6e5b4a8266be5fd8dbad7c'),
-        "experiences":1000,
+        "experiences":[new ObjectId('5c8e342f7bcd6d2b56114945'), new ObjectId('5c8e343680ee502b6704abfe')],
         "verified":true,
         "user":new ObjectId('5c8e067751ca6c325c34b2db'),
         "createdAt": faker.date.past(),
