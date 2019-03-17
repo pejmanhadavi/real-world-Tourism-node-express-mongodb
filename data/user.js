f9243a8fe7f3425b4aba0903e183a0484f7100a5
Pejman Hadavi <pejmanhadaviph@yahoo.com>
2019-03-17 12:06:04 +0330

    add user data

---
+++ data/user.js
@@ -0,0 +1,57 @@
+const faker = require('faker');
+const ObjectId = require('mongoose').Schema.Types.ObjectId;
+const uuid = require('uuid');
+
+module.exports = [
+    {
+        _id: new ObjectId('5c8e06552bc3613245d690ed'),
+        verified:true,
+        loginAttempts:0,
+        profileImages:[],
+        languages:[],
+        iWillShowYou:[],
+        travelFacilities:[],
+        name:"Super Administrator",
+        //password is admin
+        password:"$2b$10$7JFz3Ah9Nnk5jwbj.gkAmOwusKA.T6XAYMrtSihxlJ/9bU2ZGmERG",
+        email:"admin@admin.com",
+        verification: uuid.v4(),
+        blockExpires: faker.date.past(),
+        createdAt: faker.date.past(),
+        updatedAt: faker.date.past()
+    },
+    {
+        _id: new ObjectId('5c8d5a4a545e5728e469cf6b'),
+        verified:true,
+        loginAttempts:0,
+        profileImages:[],
+        languages:[],
+        iWillShowYou:[],
+        travelFacilities:[],
+        name:"user",
+        //password is admin
+        password:"$2b$10$7JFz3Ah9Nnk5jwbj.gkAmOwusKA.T6XAYMrtSihxlJ/9bU2ZGmERG",
+        email:"user@example.com",
+        verification: uuid.v4(),
+        blockExpires: faker.date.past(),
+        createdAt: faker.date.past(),
+        updatedAt: faker.date.past()
+    },
+    {
+        _id: new ObjectId('5c8e067751ca6c325c34b2db'),
+        verified:true,
+        loginAttempts:0,
+        profileImages:[],
+        languages:[],
+        iWillShowYou:[],
+        travelFacilities:[],
+        name:"Leader",
+        //password is admin
+        password:"$2b$10$7JFz3Ah9Nnk5jwbj.gkAmOwusKA.T6XAYMrtSihxlJ/9bU2ZGmERG",
+        email:"leader@example.com",
+        verification: uuid.v4(),
+        blockExpires: faker.date.past(),
+        createdAt: faker.date.past(),
+        updatedAt: faker.date.past()
+    }
+];
