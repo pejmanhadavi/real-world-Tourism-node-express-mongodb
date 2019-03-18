44fac58a619d12a2b86f537d3c2129b85c4606e9
Pejman Hadavi <pejmanhadaviph@yahoo.com>
2019-03-18 23:04:46 +0330

    complete profile test

--- test/profile.js
+++ test/profile.js
@@ -3,10 +3,16 @@ process.env.NODE_ENV = 'test';
 const chai = require('chai');
 const chaiHttp = require('chai-http');
 
+const should = chai.should();
 
 const loginDetails = {
-	email: 'pejmanhadavi77@gmail.com',
+	email: 'admin@admin.com',
 	password: 'admin'
+};
+
+const editPassDetails = {
+	currentPassword: 'admin',
+	newPassword: 'admin'
 };
 
 const server = require('../bin/www').server;
@@ -15,6 +21,11 @@ let token;
 
 chai.use(chaiHttp);
 
+before(done => {
+	setTimeout(() => {
+		done();
+	}, 1000);
+});
 
 
 describe('*********** PROFILE ***********', () => {
@@ -26,8 +37,8 @@ describe('*********** PROFILE ***********', () => {
 				.send(loginDetails)
 				.end((err, res) => {
 					res.should.have.status(200);
-					res.body.should.have.property('token');
-					token = res.body.token;
+					res.body.data.should.have.property('token');
+					token = res.body.data.token;
 					done();
 				});
 		});
@@ -50,13 +61,28 @@ describe('*********** PROFILE ***********', () => {
 				.end((err, res) => {
 					res.should.have.status(200);
 					res.body.should.be.an('object');
-					res.body.should.include.keys('name', 'email');
+					res.body.data.should.include.keys('name', 'email');
 					done();
 				});
 		});
 	});
 
 	describe('/PUT profile', () => {
+		it('it should NOT UPDATE profile since no token was sent', done => {
+			const user = {
+				name: 'Test123456',
+			};
+			chai
+				.request(server)
+				.put('/profile')
+				.send(user)
+				.end((err, res) => {
+					res.should.have.status(401);
+					res.body.should.be.a('object');
+					done();
+				});
+		});
+
 		it('it should UPDATE profile', done => {
 			const user = {
 				name: 'Test123456',
@@ -69,6 +95,44 @@ describe('*********** PROFILE ***********', () => {
 				.end((err, res) => {
 					res.should.have.status(200);
 					res.body.should.be.a('object');
+					done();
+				});
+		});
+	});
+
+	describe('/PUT password', () => {
+		it('should NOT UPDATE password since not token was sent', done =>{
+			chai
+				.request(server)
+				.put('/profile/password')
+				.send(editPassDetails)
+				.end((err, res) => {
+					res.should.have.status(401);
+					done();
+				});
+		});
+		it('should NOT UPDATE password when current password is incurrect', done => {
+			chai
+				.request(server)
+				.put('/profile/password')
+				.set('Authorization', `Bearer ${token}`)
+				.send({
+					currentPassword: 'wrongPass',
+					newPassword: 'new for wrongPass'
+				})
+				.end((err, res) => {
+					res.should.have.status(409);
+					done();
+				});
+		});
+		it('should UPDATE password', done => {
+			chai
+				.request(server)
+				.put('/profile/password')
+				.set('Authorization', `Bearer ${token}`)
+				.send(editPassDetails)
+				.end((err, res) => {
+					res.should.have.status(200);
 					done();
 				});
 		});
