7b3ff8a8ab503e3a82b1fe5b5ba58f3b301e73ad
Pejman Hadavi <pejmanhadaviph@yahoo.com>
2019-03-20 19:01:17 +0330

    edit double cotations from tour leader data

--- data/tourleaders/tourLeader.js
+++ data/tourleaders/tourLeader.js
@@ -3,11 +3,11 @@ const ObjectId = require('mongoose').Types.ObjectId;
 
 module.exports =[
     {
-        _id:new ObjectId('5c6e5b4a8266be5fd8dbad7c'),
-        "experiences":[new ObjectId('5c8e342f7bcd6d2b56114945'), new ObjectId('5c8e343680ee502b6704abfe')],
-        "verified":true,
-        "user":new ObjectId('5c8e067751ca6c325c34b2db'),
-        "createdAt": faker.date.past(),
-        "updatedAt": faker.date.recent()
+        _id: new ObjectId('5c6e5b4a8266be5fd8dbad7c'),
+        experiences: [new ObjectId('5c8e342f7bcd6d2b56114945'), new ObjectId('5c8e343680ee502b6704abfe')],
+        verified: true,
+        user: new ObjectId('5c8e067751ca6c325c34b2db'),
+        createdAt: faker.date.past(),
+        updatedAt: faker.date.recent()
     }
     ];
