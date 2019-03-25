433383feac347f44a6c66385b431f6c8dd5dd343
Pejman Hadavi <pejmanhadaviph@yahoo.com>
2019-03-25 18:14:38 +0430

    add seed and province data

---
+++ data/provinces/province.js
@@ -0,0 +1,19 @@
+const faker = require('faker');
+const ObjectId = require('mongoose').Types.ObjectId;
+const provinces = [];
+let province;
+for( let i = 0 ; i < 20 ; i ++ ) {
+
+  province = {
+    _id: new ObjectId(),
+    name: faker.address.state()
+  };
+
+  provinces.push(province);
+
+}
+
+console.log(provinces.map(a => a._id));
+
+module.exports = provinces;
+module.exports.ids = provinces.map(a => a._id);
