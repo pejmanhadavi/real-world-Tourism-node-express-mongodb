0a9f08649d2c1aa946682b5272a86ab1d518040e
Pejman Hadavi <pejmanhadaviph@yahoo.com>
2019-03-25 17:40:19 +0430

    edit data path

--- test/seed.js
+++ test/seed.js
@@ -8,7 +8,7 @@ const config = {
 };
 
 const seeder = new Seeder(config);
-const collections = seeder.readCollectionsFromPath(path.resolve('./data'));
+const collections = seeder.readCollectionsFromPath(path.resolve('./test/data'));
 
 
 const main = async () => {
