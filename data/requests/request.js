a004c140f8ff83177a805dca5dc320f9031b4023
Pejman Hadavi <pejmanhadaviph@yahoo.com>
2019-03-25 19:09:05 +0430

    add request data

---
+++ data/requests/request.js
@@ -0,0 +1,34 @@
+const faker = require('faker');
+const ObjectId = require('mongoose').Types.ObjectId;
+const requests = [];
+let request;
+const users = require('../users/user').ids;
+const leaders = require('../tourleaders/tourleader').ids;
+const experiences = require('../experiences/experience').ids;
+
+for( let i = 0 ; i < 20 ; i ++ ) {
+    request = {
+        _id: new ObjectId(),
+        user: new ObjectId(users[i]),
+        tourLeader: new ObjectId(leaders[20-i]),
+        experiences: [
+            new ObjectId(experiences[Math.floor(Math.random() * 20)]),
+            new ObjectId(experiences[Math.floor(Math.random() * 20)]),
+            new ObjectId(experiences[Math.floor(Math.random() * 20)]),
+        ],
+        tourLeaderUserId: users[i],
+        tourLeaderFirstValidate: true,
+        userFinalValidate: true,
+        tourLeaderFinalValidate: true,
+        paid: true,
+        userSatisfaction: true,
+        tourLeaderSatisfaction: true,
+        rated: true,
+    };
+
+    requests.push(request);
+
+}
+
+module.exports = requests;
+module.exports.ids = requests.map(a => a._id);
