b0161a5fc61be0f5b89d13f0009bfc8d7bde52e4
Pejman Hadavi <pejmanhadaviph@yahoo.com>
2019-03-28 16:19:36 +0430

    uncomment handle error

--- app.js
+++ app.js
@@ -40,7 +40,7 @@ app.use(function(err, req, res, next) {
 	// render the error page
 	res.status(err.status || 500);
 	console.log(err);
-	// handleError(res, buildErrObject(err.code, err.message));
+	handleError(res, buildErrObject(err.code, err.message));
 
 });
 
